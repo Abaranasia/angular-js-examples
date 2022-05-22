1b68f26f46343b3d2719b5327a1b0712be6815b7
Ran Kirlian <rankirlian@gmail.com>
2022-05-22 09:10:03 +0200

    Add external html template

--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 **Serve with**
 ./src/python -m SimpleHTTPServer 5000
+
+or (on Python 3)
+
+python -m http.server 5000 
--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -3,8 +3,8 @@
 angular.module('blogList')
 .component('blogList', { // A component includes a name, to be refered to in the view, a template with the content and the controller with the component code
   
-  template: '<div class=""><h2>{{ message }}</h2><button ng-click="buttonHandle()">Click me</button></div>', // Template with the component view
-  
+  //template: '<div class=""><h2>{{ message }}</h2><button ng-click="buttonHandle()">Click me</button></div>', // Template with the component view
+  templateUrl: '/templates/blog-list.html',
   controller: function($scope) { // Functionality of the component
     console.log(`I'm a controller`);
     $scope.message="Blog-list is running...";
