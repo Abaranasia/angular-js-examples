769ad6b981135ee0959973d1f1ef72dd6612e15a
Ran Kirlian <rankirlian@gmail.com>
2022-05-28 19:38:02 +0200

    Update Blog-list comments

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,7 +1,11 @@
 'use strict';
+/** Components are defined on the module, where all parts are described.  
+ * A component includes a name, to be refered to in the view, 
+ * a template with the content, which usually relies on an html file
+ * and the controller that contains all the component code */
 
 angular.module('blogList')
-.component('blogList', { // A component includes a name, to be refered to in the view, a template with the content and the controller with the component code
+  .component('blogList', { 
   
   //template: '<div class=""><h2>{{ message }}</h2><button ng-click="buttonHandle()">Click me</button></div>', // Template with the component view
   templateUrl: '/templates/blog-list.html',
