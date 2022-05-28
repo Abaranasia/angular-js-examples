bb73266c2fc917d6a776931973654eea9ef822d2
Ran Kirlian <rankirlian@gmail.com>
2022-05-28 19:49:44 +0200

    Update first-page component with comments

--- src/js/app/first-page/first-page.component.js
+++ src/js/app/first-page/first-page.component.js
@@ -5,5 +5,7 @@ angular.module('firstPage')
     templateUrl: '/templates/first-page.html',
     controller: function ($scope) {
       console.log(`I'm First Page controller`);
+      // No more code here because the main functionality of this component
+      // is based on the directives, which act directly in the template
     }
   })
