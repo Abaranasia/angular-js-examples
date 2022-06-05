d19b5c3a86483706a8da3029644a4342341148d5
Ran Kirlian <rankirlian@gmail.com>
2022-06-05 12:34:28 +0200

    Update blog-entry for saferty

--- src/js/app/blog-entry/blog-entry.component.js
+++ src/js/app/blog-entry/blog-entry.component.js
@@ -6,13 +6,28 @@ angular.module('blogEntry')
     templateUrl: '/templates/blog-entry.html',
     controller: function ($routeParams, $scope) { // Functionality of the component
       console.log(`I'm Blog Entry view`);
-      console.log($routeParams)
+      console.log("$routeParams", $routeParams)
       $scope.entryId = '';
       $scope.title = "No entries available";
 
-      if ($routeParams) {
+      // Safer version, asuming we have access to this info from the detail (like from a DB for example)
+      let blogItems = [
+        { title: 'Entry-1', id: 1, desc: 'Benefits of tea' },
+        { title: 'Entry-2', id: 2, desc: 'Tasty veggie meals' },
+        { title: 'Entry-3', id: 3, desc: 'React vs Angular vs Vuejs' },
+        { title: 'Entry-4', id: 4, desc: 'Learn Node AND Python' },
+      ];
+
+      angular.forEach(blogItems, (blog) => {
+        if (blog.id == $routeParams.id) { // ==, not === unless we use parseInt on the routeParam
+          $scope.entryId = blog.id;
+          $scope.title = blog.desc
+        }
+
+      });
+/*       if ($routeParams) {
         $scope.entryId = $routeParams.id;
         $scope.title = $routeParams.t;
-      }
+      } */
     }
   })
