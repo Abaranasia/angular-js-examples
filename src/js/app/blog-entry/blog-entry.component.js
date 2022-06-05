20a784f1a95a3a66d384a94e1c0caf1ff6b1ce03
Ran Kirlian <rankirlian@gmail.com>
2022-06-05 14:38:45 +0200

    Refactor blog-entry to a modern promise handler flow

--- src/js/app/blog-entry/blog-entry.component.js
+++ src/js/app/blog-entry/blog-entry.component.js
@@ -11,39 +11,21 @@ angular.module('blogEntry')
       $scope.title = "No entries available";
       $scope.blogItems = [];
 
-      const successCallback = (response) => {
-        console.log(response)
-        $scope.blogItems = res.data
-        angular.forEach($scope.blogItems, (blog) => {
-          if (blog.id == $routeParams.id) { // ==, not === unless we use parseInt on the routeParam
-            $scope.entryId = blog.id;
-            $scope.title = blog.desc
-          }
-        });
-      };
-
-      const failureCallback = (response) => {
-        console.log("Error: ", response.statusText)
-      }
-
       // V3: reading from a served JSON file:
-      $http.get("/json/blogPosts.json") // classic old-styled angular way
-        .then(successCallback, failureCallback)
+      $http.get("/json/blogPosts.json") // modern promise handler
+        .then(({ data }) => {
+          $scope.blogItems = data;
 
+          angular.forEach($scope.blogItems, (blog) => {
+            if (blog.id == parseInt($routeParams.id)) {
+              $scope.entryId = blog.id;
+              $scope.title = blog.desc
+            }
+          });
+        })
+        .catch((error) => {
+          console.err("Error: ", error)
+        });
 
-
-      // Safer version, asuming we have access to this info from the detail (like from a DB for example)
-      /*       let blogItems = [
-              { title: 'Entry-1', id: 1, desc: 'Benefits of tea' },
-              { title: 'Entry-2', id: 2, desc: 'Tasty veggie meals' },
-              { title: 'Entry-3', id: 3, desc: 'React vs Angular vs Vuejs' },
-              { title: 'Entry-4', id: 4, desc: 'Learn Node AND Python' },
-            ]; */
-
-
-      /*       if ($routeParams) {
-              $scope.entryId = $routeParams.id;
-              $scope.title = $routeParams.t;
-            } */
     }
   })
