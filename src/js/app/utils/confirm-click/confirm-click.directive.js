cb16c4f7318b7eafea68dfb8190c8844a173b4c7
Ran Kirlian <rankirlian@gmail.com>
2022-06-06 23:47:12 +0200

    Update confirm-click directive to be a tag element instead of an attribute

--- src/js/app/utils/confirm-click/confirm-click.directive.js
+++ src/js/app/utils/confirm-click/confirm-click.directive.js
@@ -3,12 +3,17 @@
 angular.module('confirmClick').
   directive('confirmClick', function () {
     return {
+      scope: {
+        message: "@message",
+      },
+      restrict: "E", // Only displays the element part of the directive
+      template: "<a href=''>visit</a>",
       link: function (scope, element, attr) {
-        console.log(scope);
+        console.log(scope.message); // It contains an attribute of the directive
         console.log(element); // Element which the directive is binded; here is an a tag
-        console.log(attr.confirmClick); // Displays the value of the confirmClick attribute of the binded element
-        console.log("ngHref: ", attr.ngHref); // Displays the  value of the ng-href attribute of the binded element
         console.log("id: ", attr.id); // Displays the  value of the id attribute of the binded element
+      //  console.log(attr.confirmClick); // Displays the value of the confirmClick attribute of the binded element
+      //  console.log("ngHref: ", attr.ngHref); // Displays the  value of the ng-href attribute of the binded element
       }
     }
   })
