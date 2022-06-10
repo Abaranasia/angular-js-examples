fd3929bc336a9bc7100beb90464dc2c391925836
Ran Kirlian <rankirlian@gmail.com>
2022-06-10 19:39:45 +0200

    update confirmClick to display scope variables

--- src/js/app/utils/confirm-click/confirm-click.directive.js
+++ src/js/app/utils/confirm-click/confirm-click.directive.js
@@ -5,13 +5,16 @@ angular.module('confirmClick').
     return {
       scope: {
         message: "@message",
+        eq: "=eq",
+        post: "=post",
       },
       restrict: "E", // Only displays the element part of the directive
-      template: "<a href=''>visit</a>",
+      template: "<a href=''>( {{ post.id }} )</a>", // It shows the title of the post, so no need to write text 
       link: function (scope, element, attr) {
         console.log(scope.message); // It contains an attribute of the directive
         console.log(element); // Element which the directive is binded; here is an a tag
         console.log("id: ", attr.id); // Displays the  value of the id attribute of the binded element
+        console.log("eq: ", scope.eq); // Displays the value of the id attribute + 1000
       //  console.log(attr.confirmClick); // Displays the value of the confirmClick attribute of the binded element
       //  console.log("ngHref: ", attr.ngHref); // Displays the  value of the ng-href attribute of the binded element
       }
