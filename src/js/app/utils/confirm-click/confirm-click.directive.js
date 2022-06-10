5353b6c0729634a181d47fc732db760cc3cd79d5
Ran Kirlian <rankirlian@gmail.com>
2022-06-10 20:29:39 +0200

    Update confirm-click to navigate to blog entry after confirm

--- src/js/app/utils/confirm-click/confirm-click.directive.js
+++ src/js/app/utils/confirm-click/confirm-click.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('confirmClick').
-  directive('confirmClick', function () {
+  directive('confirmClick', function ($rootScope, $location) {
     return {
       scope: {
         message: "@message",
@@ -9,20 +9,29 @@ angular.module('confirmClick').
         post: "=post",
       },
       restrict: "E", // Only displays the element part of the directive
-      template: "<a href=''>( {{ post.id }} )</a>", // It shows the title of the post, so no need to write text
+      template: '<a ng-href="#">{{ post.title }} - {{ post.desc }} </a>',
       /*
-      template: <a ng-href="/entry/{{ post.id }}">{{ post.title }} </a>",
       This will display the entire link fron a template instead of writing it in the component,
       so it will act as a component itself ready to be inserted everywhere
-      
        */
       link: function (scope, element, attr) {
-        console.log(scope.message); // It contains an attribute of the directive
-        console.log(element); // Element which the directive is binded; here is an a tag
-        console.log("id: ", attr.id); // Displays the  value of the id attribute of the binded element
-        console.log("eq: ", scope.eq); // Displays the value of the id attribute + 1000
+      //  console.log(scope.message); // It contains an attribute of the directive
+      // console.log(element); // Element which the directive is binded; here is an a tag
       //  console.log(attr.confirmClick); // Displays the value of the confirmClick attribute of the binded element
       //  console.log("ngHref: ", attr.ngHref); // Displays the  value of the ng-href attribute of the binded element
+        console.log("id: ", attr.id); // Displays the  value of the id attribute of the binded element
+        console.log("eq: ", scope.eq); // Displays the value of the id attribute + 1000
+
+        var msg = scope.message || "Are you sure?"
+        element.bind("click", function (event) {
+          if (window.confirm(msg)) {
+            console.log('/entry/' + scope.post.id);
+            $rootScope.$apply(function () {
+              $location.path('/entry/' + scope.post.id)
+            })
+          }
+        });
+
       }
     }
   })
